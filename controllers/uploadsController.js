0f17a4f18d97159830523d070faadfd4dca83ccb
Aymen <aymenghaloua@gmail.com>
2023-12-12 04:03:53 +0100

    first commit

---
+++ controllers/uploadsController.js
@@ -0,0 +1,8 @@
+const Product = require('../models/Product');
+const { StatusCodes } = require('http-status-codes');
+
+const uploadProductImage = async (req, res) => {
+  res.send('create product');
+};
+
+module.exports = { uploadProductImage };
