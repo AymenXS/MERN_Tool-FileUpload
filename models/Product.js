c696c8e1235ce2ca895d76ebe3df8ad641388c5a
Aymen <aymenghaloua@gmail.com>
2023-12-31 02:01:15 +0100

    UPDATING: Fixing Upload Bug.

--- models/Product.js
+++ models/Product.js
@@ -5,12 +5,10 @@ const ProductSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-
   price: {
     type: Number,
     required: true,
   },
-
   image: {
     type: String,
     required: true,
